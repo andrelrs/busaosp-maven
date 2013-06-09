537f8e3f4c289ca34a203f5d1105bc514c880cb9
André Silva <macolinkalkin@gmail.com>
2013-06-09 03:31:37 -0300

    Deixando bem legal

--- src/br/com/caelum/ondeestaobusao/fragments/PontosProximosFragment.java
+++ src/br/com/caelum/ondeestaobusao/fragments/PontosProximosFragment.java
@@ -1,104 +1,111 @@
 package br.com.caelum.ondeestaobusao.fragments;
 
-import java.util.List;
-
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.ActionMode;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.AbsListView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.emilsjolander.components.stickylistheaders.StickyListHeadersListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.caelum.ondeestaobusao.activity.MainActivity;
 import br.com.caelum.ondeestaobusao.activity.R;
-import br.com.caelum.ondeestaobusao.activity.application.BusaoApplication;
 import br.com.caelum.ondeestaobusao.adapter.PontosEOnibusAdapter;
 import br.com.caelum.ondeestaobusao.model.Onibus;
 import br.com.caelum.ondeestaobusao.model.Ponto;
-import br.com.caelum.ondeestaobusao.task.BuscaPontosDoOnibusTask;
-import br.com.caelum.ondeestaobusao.widget.PontoExpandableListView;
 
 public class PontosProximosFragment extends Fragment {
 	/*private PontoExpandableListView pontosExpandableListView;*/
 
     private StickyListHeadersListView pontosListView;
-    private ActionMode actionMode;
-    private int numero = 1;
+    private ArrayList<Onibus> selecionados;
+    private TextView actionModeCustomView;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.onibuses_proximos, null);
+        actionModeCustomView = new TextView(getActivity());
+        if (savedInstanceState == null) {
+            this.selecionados = new ArrayList<Onibus>();
+        } else {
+            this.selecionados = (ArrayList<Onibus>) savedInstanceState.getSerializable("selecionados");
+            actionModeCustomView.setText(String.valueOf(this.selecionados.size()));
+        }
+
+        View view = inflater.inflate(R.layout.onibuses_proximos, null);
 
         pontosListView = (StickyListHeadersListView) view.findViewById(R.id.listPonto);
+        pontosListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
 
         List<Ponto> pontos = ((MainActivity) getActivity()).getPontos();
-		if (pontos!= null) {
-			colocaNaTela(pontos);
-		}
+        if (pontos!= null) {
+            colocaNaTela(pontos);
+        }
 
-        final TextView textView = new TextView(getActivity());
+        return view;
+    }
 
-        pontosListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (actionMode == null) {
-                    actionMode = getActivity().startActionMode(callback);
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
 
-                    actionMode.setCustomView(textView);
-                }
+        outState.putSerializable("selecionados", selecionados);
+    }
 
-
-                view.setSelected(!view.isSelected());
-
-                if (view.isSelected()) {
-                    textView.setText(String.valueOf(numero++));
-
-                } else {
-                    textView.setText(String.valueOf(numero--));
-                }
-            }
-        });
-		
-		return view;
-	}
-	
-	public void colocaNaTela(final List<Ponto> pontos) {
+    public void colocaNaTela(final List<Ponto> pontos) {
         PontosEOnibusAdapter adapter = new PontosEOnibusAdapter(pontos, getActivity());
         pontosListView.setAdapter(adapter);
-	}
+        pontosListView.setMultiChoiceModeListener(callback(adapter));
+    }
 
-    private ActionMode.Callback callback = new ActionMode.Callback() {
-        @Override
-        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-            return true;
-        }
+    private AbsListView.MultiChoiceModeListener callback(final PontosEOnibusAdapter adapter) {
+        return new AbsListView.MultiChoiceModeListener() {
 
-        @Override
-        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-            return false;
-        }
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int position, long l, boolean checked) {
+                Onibus onibus = (Onibus) adapter.getItem(position);
+                if (checked) {
+                    selecionados.add(onibus);
+                } else {
+                    selecionados.remove(onibus);
+                }
 
-        @Override
-        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-            actionMode.finish();
-            return true;
-        }
+                actionModeCustomView.setText(String.valueOf(selecionados.size()));
+            }
 
-        @Override
-        public void onDestroyActionMode(ActionMode actionMode) {
-            PontosProximosFragment.this.actionMode = null;
-            numero = 1;
-        }
-    };
-	
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                actionMode.setCustomView(actionModeCustomView);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
+            }
+        };
+    }
+
+
+
 }
